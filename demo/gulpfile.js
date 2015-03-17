722d13cca813944e60d962ec1f067708540308cd
Dan Ross <ross9885@gmail.com>
2015-03-17 09:36:35 +0300

    Trivial React component demo

---
+++ demo/gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require( 'gulp' );
+var gutil = require( 'gulp-util' );
+var watchify = require( 'watchify' );
+var browserify = require( 'browserify' );
+var source = require( 'vinyl-source-stream' );
+var buffer = require( 'vinyl-buffer' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+
+gulp.task( 'html', function() {
+	gulp.src( 'src/index.html' )
+	.pipe( gulp.dest( 'dist' ));
+});
+
+// Watchify helps Browserify to only rebuild the parts of a source tree that are affected by a change, to reduce build time.
+// See https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
+var bundler = watchify( browserify( './src/index.js', watchify.args ));
+bundler.transform( 'reactify' );
+
+gulp.task( 'watch', [ 'html' ], bundle ); // `gulp demo:build` to start the build
+bundler.on( 'update', bundle ); // On any dependency update, run the bundler
+bundler.on( 'log', gutil.log ); // Help bundler log to the terminal
+
+function bundle() {
+	return bundler.bundle()
+	.on( 'error', gutil.log.bind( gutil, 'Browserify error' )) // Log errors during build
+	.pipe( source( 'index.min.js' ))
+	.pipe( buffer() )
+	.pipe( sourcemaps.init({ loadMaps: true }))
+	.pipe( gulp.dest( './dist' ));
+}
--- readme.md
+++ readme.md
@@ -4,6 +4,20 @@ A seed project for front end modules, with automated build and tests.
 
 ## Tooling
 
+
 **jshint** [sublime module](https://github.com/victorporof/Sublime-JSHint) - Flag common JS errors.
 
-**jscs** [sublime module](https://github.com/SublimeLinter/SublimeLinter-jscs/) - Editor support for code style conventions.
+**jscs** [sublime module](https://github.com/SublimeLinter/SublimeLinter-jscs/) - Editor support for code style conventions. (Not currently working with JSX, do not use)
+
+## Demo
+
+From `demo/dist`, run `gulp watch` to start the build. It will rebuild whenever there are changes.
+
+Start a simple webserver in the demo dir to view the demo:
+
+```
+$ pythom -m SimpleHTTPServer
+Serving HTTP on 0.0.0.0 port 8000 ...
+```
+
+Open a browser at localhost:8000.
