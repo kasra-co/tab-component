56de71646de920ba818298a13425fe48527a0da8
William Akehurst <william@kasra.co>
2015-03-18 11:31:58 +0300

    Added Tab Components. Using ExampleComponent to render inside of the panels.

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,11 +2,14 @@ var React = require( 'react' );
 
 // Require index.js from the root of the project. That is where our module's interface is specified.
 var ExampleComponent = require( '../..' ).ExampleComponent;
+var TabList = require( '../..' ).TabList;
+var TabPanels = require( '../..' ).TabPanels;
 
 var demo = (
-	<ExampleComponent message='some demo data, just a string in this case'>
-		<strong>A React front end module</strong>
-	</ExampleComponent>
+	<div>
+		<TabList tabLabels={['1','2','3','4']} />
+		<TabPanels tabPanels={[<ExampleComponent />,<ExampleComponent />,<ExampleComponent />,<ExampleComponent />]} />
+	</div>
 );
 
 React.render( demo, document.body );
