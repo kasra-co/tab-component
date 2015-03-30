e4dd85cee3cf60016d59dafd1fb66c85c2b25e1c
William Akehurst <william@kasra.co>
2015-03-30 08:47:53 +0300

    Removed store and actions. Component now uses a select function with a callback to select tabs and panels.

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,14 +2,18 @@ var React = require( 'react' );
 
 // Require index.js from the root of the project. That is where our module's interface is specified.
 var ExampleComponent = require( '../..' ).ExampleComponent;
-var TabList = require( '../..' ).TabList;
-var TabPanels = require( '../..' ).TabPanels;
+var TabView = require( '../..' ).TabView;
 
 var demo = (
 	<div>
-		<TabList tabLabels={['1','2','3','4']} />
-		<TabPanels tabPanels={[<ExampleComponent />,<ExampleComponent />,<ExampleComponent />,<ExampleComponent />]} />
+		<TabView
+		tabLabels={['1','2','3','4']}
+		tabPanels={[<p>1a</p>,<p>2a</p>,<p>3a</p>,<p>4a</p>]} />
+		<TabView
+		tabLabels={['1','2','3','4']}
+		tabPanels={[<p>1b</p>,<p>2b</p>,<p>3b</p>,<p>4b</p>]} />
 	</div>
+
 );
 
 React.render( demo, document.body );
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ It should only export objects from the ./lib directory.
 /* A simple message view. Props:
 - children
 - message: an optional message that will be shown above the child components.  */
-exports.ExampleComponent = require( './lib/example-component' );
 exports.TabList = require( './lib/tab-list' );
 exports.TabPanels = require( './lib/tab-panels' );
+exports.TabView = require( './lib/tab-view' );
 
 /*
 exports.storeName = {} OR exports.stores = {} if there are more than one
